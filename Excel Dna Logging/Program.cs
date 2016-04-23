2da0f3449b697059be5f933d1258e49f19a3759a
crisipix <wongc5@gmail.com>
2016-04-22 22:47:19 -0400

    Initial Test

---
+++ Excel Dna Logging/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Dna_Logging
+{
+    /// <summary>
+    /// https://github.com/Excel-DNA/ExcelDna/wiki/Diagnostic-Logging
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Excel.Dna.Diagnostics/ExcelAddin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using ExcelDna.Integration.CustomUI;
+using System.Runtime.InteropServices;
+
+namespace Excel.Dna.Diagnostics
+{
+    [ComVisible(true)]
+    public class ExcelAddin : ExcelRibbon, IExcelAddIn
+    {
+        private IRibbonUI ribbon = null;
+
+        public ExcelAddin() {
+            Console.WriteLine("Hello");
+        
+        }
+
+        public void OnLoad(IRibbonUI ribbon)
+        {
+            this.ribbon = ribbon;
+        }
+
+
+        public void RunTest(IRibbonControl control)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void AutoClose()
+        {
+             
+        }
+
+        public void AutoOpen()
+        {
+            Console.WriteLine("Hello");
+             
+        }
+
+        /*
+         * "C:\Users\Chris W\Documents\GitHub\ExcelDnaTest\Excel.Dna.Diagnostics\bin\Debug\Excel.Dna.Diagnostics-AddIn.xll"
+         * 
+         * C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE
+         * 
+         xcopy "$(SolutionDir)\packages\ExcelDna.AddIn.0.33.9\tools\ExcelDna.xll" "$(TargetDir)Excel.Dna.Diagnostics-AddIn.xll*" /C /Y
+        xcopy "$(TargetDir)Excel.Dna.Diagnostics-AddIn.dna*" "$(TargetDir)Excel.Dna.Diagnostics-AddIn64.dna*" /C /Y
+        xcopy "$(SolutionDir)\packages\ExcelDna.AddIn.0.33.9\tools\ExcelDna64.xll" "$(TargetDir)Excel.Dna.Diagnostics-AddIn64.xll*" /C /Y
+        "$(SolutionDir)\packages\ExcelDna.AddIn.0.33.9\tools\ExcelDnaPack.exe" "$(TargetDir)Excel.Dna.Diagnostics-AddIn.dna" /Y
+        "$(SolutionDir)\packages\ExcelDna.AddIn.0.33.9\tools\ExcelDnaPack.exe" "$(TargetDir)Excel.Dna.Diagnostics-AddIn64.dna" /Y
+         
+         * 
+         * https://groups.google.com/forum/#!topic/exceldna/IhqXaK-avWg
+         * 
+         * xcopy "$(SolutionDir)packages\ExcelDna.AddIn.0.33.9\tools\ExcelDna.xll" "$(TargetDir)Excel.Dna.Diagnostics-AddIn.xll*" /C /Y
+            "$(SolutionDir)packages\ExcelDna.AddIn.0.33.9\tools\ExcelDnaPack.exe" "$(TargetDir)Excel.Dna.Diagnostics-AddIn.dna" /Y
+
+         * 
+         * https://msdn.microsoft.com/en-us/library/aa730920%28v=office.12%29.aspx
+         */
+    }
+}
