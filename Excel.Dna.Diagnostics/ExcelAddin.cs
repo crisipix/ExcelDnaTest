3db6c7baba74bcf55fff3fd75615d889b85fe4d0
crisipix <wongc5@gmail.com>
2016-05-24 22:45:14 -0400

    Added examples of cell ranges etc.

--- Excel.Dna.Diagnostics/ExcelAddin.cs
+++ Excel.Dna.Diagnostics/ExcelAddin.cs
@@ -40,6 +40,97 @@ namespace Excel.Dna.Diagnostics
         public void RunTest(IRibbonControl control)
         {
             Console.WriteLine("Hello");
+            MakeGraph();
+            FillColors();
+        }
+
+        /*
+         http://stackoverflow.com/questions/11223641/how-do-i-create-a-new-worksheet-and-populate-it-with-rows-of-data-using-excel-dn
+         */
+        private void MakeGraph()
+        {
+            Application app = (Application)ExcelDnaUtil.Application;
+            //var app = new Application();
+            app.Visible = true;
+            var workbook = app.Workbooks.Add(1);
+
+            Sheets excelSheets = workbook.Worksheets;
+            string currentSheet = "Sheet1";
+            Worksheet worksheet1 = (Worksheet)excelSheets.get_Item(currentSheet);
+
+
+            worksheet1.Cells[1, 1] = "";
+            worksheet1.Cells[1, 2] = "Year 1";
+            worksheet1.Cells[1, 3] = "Year 2";
+            worksheet1.Cells[1, 4] = "Year 3";
+            worksheet1.Cells[1, 5] = "Year 4";
+            worksheet1.Cells[1, 6] = "Year 5";
+
+            worksheet1.Cells[2, 1] = "Company A";
+            worksheet1.Cells[2, 2] = "10";
+            worksheet1.Cells[2, 3] = "50";
+            worksheet1.Cells[2, 4] = "70";
+            worksheet1.Cells[2, 5] = "70";
+            worksheet1.Cells[2, 6] = "70";
+
+            worksheet1.Cells[3, 1] = "Company B";
+            worksheet1.Cells[3, 2] = "30";
+            worksheet1.Cells[3, 3] = "70";
+            worksheet1.Cells[3, 4] = "80";
+            worksheet1.Cells[3, 5] = "80";
+            worksheet1.Cells[3, 6] = "80";
+
+            worksheet1.Cells[4, 1] = "Company C";
+            worksheet1.Cells[4, 2] = "55";
+            worksheet1.Cells[4, 3] = "65";
+            worksheet1.Cells[4, 4] = "75";
+            worksheet1.Cells[4, 5] = "75";
+            worksheet1.Cells[4, 6] = "75";
+
+            ChartObjects xlCharts = (ChartObjects)worksheet1.ChartObjects(Type.Missing);
+            ChartObject myChart = (ChartObject)xlCharts.Add(60, 10, 300, 300);
+            Range chartRange = worksheet1.get_Range("A1", "F4");
+
+            Chart chartPage = myChart.Chart;
+            chartPage.SetSourceData(chartRange, System.Reflection.Missing.Value);
+            chartPage.ChartType = XlChartType.xlLine;
+
+
+        }
+
+        private void FillColors()
+        {
+            Application app = (Application)ExcelDnaUtil.Application;
+            //var app = new Application();
+            app.Visible = true;
+            var workbook = app.ActiveWorkbook;
+
+            Sheets excelSheets = workbook.Worksheets;
+            string currentSheet = "Sheet1";
+            Worksheet worksheet1 = (Worksheet)excelSheets.get_Item(currentSheet);
+            worksheet1.Cells[6, 1] = "April 1st";
+            worksheet1.Cells[6, 2] = "April 2nd";
+            worksheet1.Cells[6, 3] = "April 3rd";
+            worksheet1.Cells[6, 4] = "April 4th";
+
+            // fill in the starting and ending range programmatically this is just an example. 
+            string startRange = "A6";
+            string endRange = "A6";
+            Range currentRange = worksheet1.get_Range(startRange, endRange);
+
+            var text = currentRange.Text.ToString();
+            int length = text.Length;
+            int index = 0;
+            if (text.Contains("st"))
+            {
+                index = text.IndexOf("st");
+            }
+            //The other checks for "nd", "rd", "th" obviously check to see a # precedes these.
+
+            if (index > 0)
+            {
+                currentRange.get_Characters(index + 1, 2).Font.Superscript = true;
+            }           
         }
 
         public void AutoClose()
